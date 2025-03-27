8285a8b81c743d656bccbc992a703c776e9fe251
Vilmer Fall <Vilmer.fall@gmail.com>
2025-03-28 00:54:36 +0100

    added draft for a call to action part in about me

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,7 @@
 const aboutMeButton = document.querySelector(".about-button");
 const contactButton = document.querySelector(".contact-button");
 const headerName = document.querySelector(".header-name-container");
+const aboutMeLink = document.getElementById("about-link");
 
 const startPage = document.getElementById("start-page");
 const aboutPage = document.getElementById("about-page");
@@ -12,20 +13,22 @@ function showAboutPage() {
 	hidePages()
 	aboutPage.classList.remove("display-none")
 	aboutPage.classList.add("display-flex")
+	
 };
 
 function showStartPage() {
 	hidePages
 	startPage.classList.remove("display-none")
 	startPage.classList.add("display-flex")
-}
+};
 
 function hidePages() {
 	startPage.classList.add("display-none")
 	startPage.classList.remove(".display-flex")
 	aboutPage.classList.add("display-none")
 	aboutPage.classList.remove(".display-flex")
-}
+	window.scrollTo(0,0); 
+};
 
 
 
@@ -37,4 +40,8 @@ aboutMeButton.addEventListener("click", () => {
 
 headerName.addEventListener("click", () => {
 	showStartPage()
+});
+
+aboutMeLink.addEventListener("click", () => {
+	showAboutPage()
 })
