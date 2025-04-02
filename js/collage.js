b9839c6bf5f72f777f2dacce31313d9b514d6b6e
Vilmer Fall <Vilmer.fall@gmail.com>
2025-04-02 14:44:17 +0200

    responsive trails

--- js/collage.js
+++ js/collage.js
@@ -2,10 +2,11 @@ const bigImage = document.getElementById("big-image")
 const smallImages = document.querySelectorAll(".small")
 const imageInfoText = document.querySelector(".collage-img-info");
 
-function collageHandler () {
-    // const bigImage = document.getElementById("big-image");
-    // const smallImages = document.querySelectorAll(".small");
+//swipe vairables
+let startTouchX = 0;
+let endTouchX = 0;
 
+function collageHandler () {
 	const imageSources = [
         "./assets/images/band-1.jpg",
         "./assets/images/fishing-1.jpg",
@@ -42,6 +43,44 @@ function collageHandler () {
             smallImage.alt = tempAlt;  
         });
     });
+
+	//swipe fucntionality
+	bigImage.addEventListener("touchstart", function(e) {
+        startTouchX = e.changedTouches[0].screenX; // Store the starting point of the touch
+    });
+
+	bigImage.addEventListener("touchend", function(e) {
+        endTouchX = e.changedTouches[0].screenX; 
+
+        // If the touch movement is significant enough, swap images
+        if (endTouchX - startTouchX > 50) {
+            nextImage(); // Swipe right (next image)
+        } else if (startTouchX - endTouchX > 50) {
+            prevImage(); // Swipe left (previous image)
+        }
+    });
+
+	//go to next image
+	function nextImage() {
+        let currentIndex = Array.from(smallImages).findIndex(img => img.src === bigImage.src);
+        currentIndex = (currentIndex + 1) % smallImages.length;
+        swapImage(smallImages[currentIndex]);
+    }
+	
+	//go to previus image
+	function prevImage() {
+        let currentIndex = Array.from(smallImages).findIndex(img => img.src === bigImage.src);
+        currentIndex = (currentIndex - 1 + smallImages.length) % smallImages.length;
+        swapImage(smallImages[currentIndex]);
+	}
+
+	//swapping images
+	function swapImage(image) {
+        bigImage.src = image.src;
+        bigImage.alt = image.alt;
+        imageInfoText.innerText = image.alt;
+    }
+
 };
 
 collageHandler()
